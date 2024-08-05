7413b253e22c30ef46a6ad4b4213c1e80a5154b0
taejun0 <xownswns@naver.com>
2024-08-06 00:02:56 +0900

    FEAT: 가족 코드입력 완성

--- src/components/MyPage/EditFamily/EditFamily.jsx
+++ src/components/MyPage/EditFamily/EditFamily.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { API } from "../../../api";
 import * as S from "./Styled";
-import { SiImessage } from "react-icons/si";
-import { RiKakaoTalkFill } from "react-icons/ri";
-import { HiLink } from "react-icons/hi";
+// import { SiImessage } from "react-icons/si";
+// import { RiKakaoTalkFill } from "react-icons/ri";
+// import { HiLink } from "react-icons/hi";
 
 const EditFamily = () => {
   const [showform, setShowform] = useState(false);
   const [familyMembers, setFamilyMembers] = useState([]);
+
+  const [code, setCode] = useState("");
+
+  const [codeform, setCodeform] = useState(false);
+  const [inviteform, setInviteform] = useState(false);
 
   const fetchFamilyData = async () => {
     try {
@@ -28,12 +33,20 @@ const EditFamily = () => {
     }
   };
 
+  const Postdata = async () => {
+    try {
+      const response = await API.post("/users/family/join/", { family_code : code });
+      console.log("Post data response:", response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     fetchFamilyData();
   }, []);
 
   const getProfilePictureUrl = (url) => {
-    console.log(url);
     if (!url) return "";
     if (url.startsWith("http")) return url;
     return `http://127.0.0.1:8000${url}`;
@@ -44,8 +57,14 @@ const EditFamily = () => {
       <S.EditFamilyContainer>
         <S.Row>
           <S.EditFamilyTitle>가족 구성원</S.EditFamilyTitle>
-          <S.AddFamily onClick={() => setShowform(true)}>구성원 초대하기 +</S.AddFamily>
+          <S.AddFamily onClick={() => setShowform(!showform)}>구성원 초대하기 +</S.AddFamily>
         </S.Row>
+        {showform && (
+          <S.SelectForm>
+            <S.Selection onClick={() => setCodeform(true)}><S.StyledPen />초대 코드 입력</S.Selection>
+            <S.Selection2 onClick={() => setInviteform(true)}><S.StyledUser />가족 구성원 초대</S.Selection2>
+          </S.SelectForm>
+        )}
         <S.EditFamilyLine />
         <S.EditFamilyList>
           {familyMembers.map((member, index) => (
@@ -60,29 +79,40 @@ const EditFamily = () => {
           ))}
         </S.EditFamilyList>
       </S.EditFamilyContainer>
-      {showform && (
+      {codeform && (
         <>
-          <S.backWrapping onClick={() => setShowform(false)} />
+          <S.backWrapping onClick={() => setCodeform(false)} />
           <S.NewEventForm>
             <S.FormContent>
-              <S.FormTitle>가족 구성원 초대하기</S.FormTitle>
-              <S.InviteCode># 326879</S.InviteCode>
-              <S.Icons>
-                <S.Iconset>
-                  <RiKakaoTalkFill size={50} style={{ color: '#371d1e', backgroundColor: '#fae100', borderRadius: '0.5rem', border: '4px solid #fae100' }} />
-                  <S.IconText>카카오톡</S.IconText>
-                </S.Iconset>
-                <S.Iconset>
-                  <SiImessage size={58} />
-                  <S.IconText>문자전송</S.IconText>
-                </S.Iconset>
-                <S.Iconset>
-                  <HiLink size={58} />
-                  <S.IconText>링크복사</S.IconText>
-                </S.Iconset>
-              </S.Icons>
+              <S.Rowdi>
+                <S.FormTitle>구성원 초대 코드 입력하기</S.FormTitle>
+                <S.StyledTimes onClick={() => setCodeform(false)}/>
+              </S.Rowdi>
+              <S.FormSubtitle>가족 구성원에게 받은 6자리 숫자를 입력해주세요.</S.FormSubtitle>
+              <S.Sharp>#</S.Sharp>
+              <S.FormInput 
+                type="text"
+                placeholder="입력"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+              <S.PostButton onClick={Postdata}>인증하기</S.PostButton>
             </S.FormContent>
           </S.NewEventForm>
+        </>
+      )}
+      {inviteform && (
+        <>
+          <S.backWrapping onClick={() => setInviteform(false)} />
+          <S.FormContent>
+            <S.Rowdi>
+              <S.FormTitle>가족 구성원 초대하기</S.FormTitle>
+              <S.StyledTimes onClick={() => setInviteform(false)} />
+            </S.Rowdi>
+            <S.FormSubtitle>우리 가족명</S.FormSubtitle>
+            <S.FormInput2 />
+            <S.inputbutton>입력</S.inputbutton>
+          </S.FormContent>
         </>
       )}
     </>
--- src/components/MyPage/EditFamily/Styled.jsx
+++ src/components/MyPage/EditFamily/Styled.jsx
@@ -1,5 +1,9 @@
 import styled, {keyframes} from 'styled-components';
 
+import { FaPen, FaUserPlus, FaTimes } from "react-icons/fa";
+
+
+
 const fadeIn = keyframes`
   from {
     opacity: 0;
@@ -9,9 +13,22 @@ const fadeIn = keyframes`
   }
 `;
 
+export const StyledTimes = styled(FaTimes)`
+  width: 30px;
+`;
+
+export const StyledPen = styled(FaPen)`
+  width: 40px;
+`;
+
+export const StyledUser = styled(FaUserPlus)`
+  width: 40px;
+`;
+
 
 export const EditFamilyContainer = styled.div`
   display: flex;
+  position: relative;
   width: 1044px;
   margin-top: 2rem;
   flex-direction: column;
@@ -170,6 +187,7 @@ export const FormContent = styled.div`
   flex-direction: column;
   padding: 2rem;
   gap: 1rem;
+  position: relative;
 `;
 
 export const FormTitle = styled.div`
@@ -236,4 +254,167 @@ export const IconText = styled.div`
   @media (max-width: 359px) {
     font-size: 0.875rem;
   }
+`;
+
+export const SelectForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 4.5rem;
+  right: 0rem;
+  width: 18rem;
+  height: 10rem;
+  
+  border-radius: 1rem;
+  border: 1px solid #D9D9D9;
+  background-color: #FFF;
+  box-shadow: 0.5px 1px 15px 0px rgba(0, 0, 0, 0.20);
+`;
+
+export const Selection = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #D9D9D9;
+  gap: 1rem;
+
+  color: #000;
+
+  text-align: center;
+  font-family: 'NanumSquareRound';
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const Selection2 = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  color: #000;
+
+  text-align: center;
+  font-family: 'NanumSquareRound';
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const FormSubtitle = styled.div`
+  color: #9F9F9F;
+
+  font-family: 'NanumSquareRound';
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const FormInput = styled.input`
+  display: flex;
+  width: 150px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 4rem 1rem 7rem;
+  margin: 1rem 5rem;
+  border-radius: 2rem;
+
+  border: none;
+  outline: none;
+
+  background-color: #EDEDED;
+
+  color: #2d539e;
+  font-family: 'NanumSquareRound';
+  font-size: 1.625rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const PostButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2D539E;
+  border-radius: 1rem;
+
+  margin: 0rem 8rem;
+  padding: 0.75rem 0rem;
+
+  color: #FFF;
+
+  text-align: center;
+  font-family: 'NanumSquareRound';
+  font-size: 1.625rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const Sharp = styled.div`
+  display: flex;
+  position: absolute;
+  font-size: 2.125rem;
+  color: #2d529e;
+  font-weight: 900;
+  top: 8.75rem;
+  left: 12rem;
+`;
+
+export const Rowdi = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const FormInput2 = styled.input`
+  display: flex;
+  width: 75%;
+  height: 5rem;
+  justify-content: center;
+  align-items: center;
+
+
+  border-radius: 0.625rem;
+  border: 1px #ABABAB;
+
+  background: #F7F8FB;
+
+  color: #ABABAB;
+
+  font-family: 'NanumSquareRound';
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const inputbutton = styled.button`
+  display: flex;
+  width: 25%;
+  height: 5rem;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 0.625rem;
+  background: #2D539E;
+
+  color: #FFF;
+
+  text-align: center;
+  font-family: 'NanumSquareRound';
+  font-size: 1.875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 `;
